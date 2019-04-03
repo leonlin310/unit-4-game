b94b14314b1e73adb1d728f58b51e46dba61b043
Leon Lin <leonlin310@gmail.com>
2019-04-02 22:35:52 -0500

    test

---
+++ javascript/game.js
@@ -0,0 +1,35 @@
+//declare our variables
+var wins = 0;
+var losses = 0;
+var points = 0;
+var targetScore = "";
+var computerNumber = "";
+
+//reset
+// resetGame();
+
+
+
+
+
+
+
+ // Code pulled from stackoverflow
+function resetGame(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    varcomputerNumber = resetGame(19,120);   
+    // console.log("COMPUTER NUMBER = ", computerNumber); wrong code?
+
+}
+
+// var computerNumber = console.log(getRandomInt(19, 120));
+
+// $("#computerNumber").append.computerNumber;
+
+
+
+    
+
+
