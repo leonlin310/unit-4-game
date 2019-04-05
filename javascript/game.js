cba83ac6d977fd82799ea487cf8643cd09c6c4eb
Leon Lin <leonlin310@gmail.com>
2019-04-04 21:46:12 -0500

    all weapons have different values

--- javascript/game.js
+++ javascript/game.js
@@ -9,69 +9,69 @@ var panVal;
 var swordVal;
 var counter = 0;
 
- // Function found on Stack Overflow for random number
-    function pickComputerNumber(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-    
-    // Function to pick random number for Crystal1
-    function pickCrystalNumber(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
+// Function found on Stack Overflow for random number
+function pickComputerNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Function to pick random number for Crystal1
+function pickCrystalNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
-        $(document).ready(function() {
+$(document).ready(function () {
 
-        $(".start-button").on("click", function(){
-            console.log("i've been touched");
-            $(this).fadeOut("slow");
-            $(".display-4").text("WHAAAATT");
-            $(".lead").text("DID THAT BUTTON JUST DISSAPEAR? ");
-            $(".exp").text("Yes. Yes it did").delay(4000).fadeOut("slow");
-            
-            resetGame();
-        }) 
-
-       
-// resetGame();
-
-
-
-// Test Logic
-
-$(document).on("keyup", function(event){
-    console.log("Key up", event.key);
-    var playerLetter = event.key;
-
-    if (playerLetter === "y"){
-        wins++
-        console.log("win");
+    $(".start-button").on("click", function () {
+        console.log("i've been touched");
+        $(this).fadeOut("slow");
+        $(".display-4").text("WHAAAATT");
+        $(".lead").text("DID THAT BUTTON JUST DISSAPEAR? ");
+        $(".exp").text("Yes. Yes it did").delay(4000).fadeOut("slow");
 
         resetGame();
-    }
+    })
 
-    else {
-        console.log("lose");
-        losses++
-    }
-updateScoreboard();
 
-});
+    // resetGame();
+
+
+
+    // Test Logic
+
+    $(document).on("keyup", function (event) {
+        console.log("Key up", event.key);
+        var playerLetter = event.key;
+
+        if (playerLetter === "y") {
+            wins++
+            console.log("win");
+
+            resetGame();
+        }
+
+        else {
+            console.log("lose");
+            losses++
+        }
+        updateScoreboard();
+
+    });
 
 
 
     //Reset the game
-    function resetGame(){
+    function resetGame() {
 
-    //  Have Computer Pick a Number       
-        computerNumber = pickComputerNumber(19,120);
+        //  Have Computer Pick a Number       
+        computerNumber = pickComputerNumber(19, 120);
         console.log("Computer Picks", computerNumber);
 
-    // Have a loop number randomly assigned for Crystals
+        // Have a loop number randomly assigned for Crystals
         hammerVal = pickCrystalNumber(1, 12);
         console.log("Hammer damage is: ", hammerVal);
         axeVal = pickCrystalNumber(1, 12);
@@ -81,40 +81,45 @@ updateScoreboard();
         swordVal = pickCrystalNumber(1, 12);
         console.log("Sword damage is: ", swordVal);
 
-    // Attach a click event to each picture and assign a value to it
-        $(".hammerPic").on("click", function() {
+        // Create IF statement so if any weapon value = another weapon value, re-run function resetGame
+        if ((hammerVal == axeVal) || (hammerVal == panVal) || (hammerVal == swordVal) || (axeVal == panVal) || (axeVal == swordVal) || (panVal == swordVal)) {
+            resetGame();
+        };
+
+        // Attach a click event to each picture and assign a value to it
+        $(".hammerPic").on("click", function () {
             counter += hammerVal;
             console.log("Hammer's current damage is: ", hammerVal);
             console.log("Cumulative damage is: ", counter);
-         });
+        });
 
-         $(".axePic").on("click", function() {
+        $(".axePic").on("click", function () {
             counter += axeVal;
             console.log("Axe's current damage is: ", axeVal);
             console.log("Cumulative damage is: ", counter);
-         });
+        });
 
-         $(".panPic").on("click", function() {
+        $(".panPic").on("click", function () {
             counter += panVal;
             console.log("Frying Pan's current damage is: ", panVal);
             console.log("Cumulative damage is: ", counter);
-         });
+        });
 
-         $(".swordPic").on("click", function() {
+        $(".swordPic").on("click", function () {
             counter += swordVal;
             console.log("Sword's current damage is: ", swordVal);
             console.log("Cumulative damage is: ", counter);
-         });
+        });
 
 
-        }
-        
-       
+    }
+
+
 
 
     // Create a function that updates the wins/losses
-    function updateScoreboard(){
+    function updateScoreboard() {
         $("#wins").text(wins);
         $("#losses").text(losses);
     }
-        });
+});
